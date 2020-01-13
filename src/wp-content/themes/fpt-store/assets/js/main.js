554aeb318584d676e8adc1a0562296e02e8cfb9c
phamday <monster@namtech.com.au>
2020-01-13 20:28:02 +0700

    update

--- src/wp-content/themes/fpt-store/assets/js/main.js
+++ src/wp-content/themes/fpt-store/assets/js/main.js
@@ -1,35 +1,43 @@
 $ = jQuery;
-$(function() {
-    $(document).ready(function(){
-        $(window).scroll(function() { 
-            let top = $(window).scrollTop();
-            if (top > 100) {
-                $('.header-desktop').addClass('change-color');
-                $('.header-mobile').addClass('change-color');
-                $('.button-bottom-scroll').addClass('show-btn-scroll');
-            }
-            else {
-                $('.header-desktop').removeClass('change-color');
-                $('.header-mobile').removeClass('change-color');
-                $('.button-bottom-scroll').removeClass('show-btn-scroll');
-            }
-            $('.button-bottom-scroll').click(function(event) {
-                $('html,body').animate({scrollTop: 0},1300);
-            });
-        });
+$(document).ready(function(){
+    $(window).scroll(function() { 
         let top = $(window).scrollTop();
         if (top > 100) {
             $('.header-desktop').addClass('change-color');
             $('.header-mobile').addClass('change-color');
+            $('.button-bottom-scroll').addClass('show-btn-scroll');
         }
         else {
             $('.header-desktop').removeClass('change-color');
             $('.header-mobile').removeClass('change-color');
+            $('.button-bottom-scroll').removeClass('show-btn-scroll');
         }
-         //header-mobile
-        $('.header-mobile .btn-bar').click(function() {
-            $(this).toggleClass('active-menu-show'),$('.header-desktop').toggleClass('menu-nav-show'),
-            $('body').toggleClass('no-Scroll'),  $('.button-bottom-scroll').toggleClass('hidden-btn-scroll');
-        });
+    });
+    let top = $(window).scrollTop();
+    if (top > 100) {
+        $('.header-desktop').addClass('change-color');
+        $('.header-mobile').addClass('change-color');
+    }
+    else {
+        $('.header-desktop').removeClass('change-color');
+        $('.header-mobile').removeClass('change-color');
+    }
+    //header-mobile
+    $('.header-mobile .btn-bar').click(function() {
+        $(this).toggleClass('active-menu-show'),$('.header-desktop').toggleClass('menu-nav-show'),
+        $('body').toggleClass('no-Scroll'),  $('.button-bottom-scroll').toggleClass('hidden-btn-scroll');
+    });
+    $('.button-bottom-scroll').click(function(event) {
+        $('html,body').animate({scrollTop: 0},700);
+    });
+    //call-slick
+    $('.home-page--banner-top').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 1300,
+        arrows: false,
+        dots: false,
+        infinite: true
     });
 });
--- src/wp-content/themes/fpt-store/templates/home.php
+++ src/wp-content/themes/fpt-store/templates/home.php
@@ -3,20 +3,112 @@
 $id = get_queried_object()->ID;
 get_header();
 ?>
-<main class="home-page" style="height:100vh; padding-top:120px">
+<main class="home-page">
     <section class="home-page--banner-top">
-        <img src="<?php echo DF_IMAGE. '/banner-home.jpg';?>" alt="banner-top">
+        <div class="content-image">
+            <img src="<?php echo DF_IMAGE. '/banner-home.jpg';?>" alt="banner-top">
+            </div>
+        <div class="content-image">
+            <img src="<?php echo DF_IMAGE. '/banner.png';?>" alt="banner-top">
+            </div>
+        <div class="content-image">
+            <img src="<?php echo DF_IMAGE. '/banner-home.jpg';?>" alt="banner-top">
+        </div>
+        <div class="content-image">
+            <img src="<?php echo DF_IMAGE. '/banner.png';?>" alt="banner-top">
+        </div>
     </section>
     <section class="home-page--service">
-        <h4 class="title-h4">DỊCH VỤ</h4>
-        <div class="line-under"></div>
-        <div class="home-page--service--cont-group">
-            <div class="row">
-                <div class="col-lg-3 col-md-4 col-sm-12 col-12 col-custom">
-                    <div class="cont-box">
-                        <div class="cont-image"></div>
-                        <div class="cont-title-service">
-                        <a href="#" ></a>
+        <div class="container">
+            <h4 class="title-h4">DỊCH VỤ</h4>
+            <div class="line-under"></div>
+            <div class="home-page--service--cont-group">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-12 col-custom">
+                        <div class="cont-box">
+                            <div class="cont-image">
+                                <a href="#"><img src="<?php echo DF_IMAGE. '/internet.jpg';?>" alt="internet"></a>
+                            </div>
+                            <div class="cont-title-service">
+                                <a href="#">INTERNET</a>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-12 col-custom">
+                        <div class="cont-box">
+                            <div class="cont-image">
+                                <a href="#"><img src="<?php echo DF_IMAGE. '/truyen-hinh.jpg';?>" alt="truyen-hinh"></a>
+                            </div>
+                            <div class="cont-title-service">
+                                <a href="#">TRUYỀN HÌNH</a>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-12 col-custom">
+                        <div class="cont-box">
+                            <div class="cont-image">
+                                <a href="#"><img src="<?php echo DF_IMAGE. '/play-box.jpg';?>" alt="play-box"></a>
+                            </div>
+                            <div class="cont-title-service">
+                                <a href="#">PLAY BOX</a>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-12 col-custom">
+                        <div class="cont-box">
+                            <div class="cont-image">
+                                <a href="#"><img src="<?php echo DF_IMAGE. '/camera.png';?>" alt="camera"></a>
+                            </div>
+                            <div class="cont-title-service">
+                                <a href="#">CAMERA</a>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="home-page--blog">
+        <div class="container">
+            <h4 class="title-h4">TIN TỨC</h4>
+            <div class="line-under"></div>
+            <div class="home-page--blog--cont-group">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-12 col-custom">
+                        <div class="cont-box">
+                            <div class="cont-image">
+                                <a href="#"><img src="<?php echo DF_IMAGE. '/internet.jpg';?>" alt="internet"></a>
+                            </div>
+                            <div class="cont-text">
+                                <a href="#">INTERNET</a>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-12 col-custom">
+                        <div class="cont-box">
+                            <div class="cont-image">
+                                <a href="#"><img src="<?php echo DF_IMAGE. '/internet.jpg';?>" alt="internet"></a>
+                            </div>
+                            <div class="cont-text">
+                                <a href="#">INTERNET</a>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-12 col-custom">
+                        <div class="cont-box">
+                            <div class="cont-image">
+                                <a href="#"><img src="<?php echo DF_IMAGE. '/internet.jpg';?>" alt="internet"></a>
+                            </div>
+                            <div class="cont-text">
+                                <a href="#">INTERNET</a>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                            </div>
                         </div>
                     </div>
                 </div>
