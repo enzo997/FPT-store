bbae96afbf1faf551ab748142643c9542ecde1ec
phamday <monster@namtech.com.au>
2020-01-18 16:24:05 +0700

    update plugin

--- src/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ src/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit5590e8cfe6420d65b8c17b52f9cf863d::getLoader();
+return ComposerAutoloaderInitcf6d0767e943c5d8398efe1a4cb2fc19::getLoader();
---
+++ src/wp-content/themes/fpt-store/single-service.php
@@ -0,0 +1,38 @@
+<?php
+get_header();
+$post = get_queried_object();
+$id = get_queried_object()->ID;
+$data_slug =  get_the_terms($id, 'categories-service');
+$slug = $data_slug[0]->slug
+?>
+    <style>
+        .header-desktop--content-header--col-right--main-nav-menu ul .menu-item-34 a{
+            color:#f06e28;
+        }
+    </style>
+    <main class="single-service" id="singl-service" style="height:100vh; padding-top:120px" data-slug = "<?php echo $slug; ?>">
+    <?php
+        if($slug == 'goi-dich-vu-internet'){
+            ?>
+                <h1 class="">INTERNET</h1>
+            <?php
+        }
+        elseif($slug == 'goi-dich-vu-truyen-hinh'){
+            ?>
+                <h1 class="">TRUYEN HINH</h1>
+            <?php
+        }
+        elseif($slug == 'fpt-play-box'){
+            ?>
+                <h1 class="">FPT PLAY BOX</h1>
+            <?php
+        }
+        else{
+            ?>
+                <h1 class="">CAMERA GIAM SAT</h1>
+            <?php
+        }
+    ?>
+    </main>
+<?php
+get_footer();
